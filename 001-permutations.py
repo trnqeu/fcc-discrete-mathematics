f5cd33ef97f6a45fd8dc8bb1bce412e4396f4602
trnq-eu <48558947+trnq-eu@users.noreply.github.com>
2025-11-16 06:33:46 +0100

    implemented permutations with itertools and defaultdict

--- 001-permutations.py
+++ 001-permutations.py
@@ -1,9 +1,28 @@
 from itertools import permutations
 from math import factorial
+from collections import defaultdict
+
+def how_many_permutations(text: str) -> int:
+    letters = dict()
+    for letter in text:
+        letters[letter] +=1
+
+    numerator = factorial(len(text))
+    factors = [factorial(count) for count in letters.values()]
+    denominator = 1
+    for factor in factors:
+        denominator *= factor
+    return numerator // denominator
 
 
-perms = {''.join(x) for x in list(permutations("barbarian"))}
-print(perms)
-print(len(perms))
+# perms = {''.join(x) for x in list(permutations("barbarian"))}
+# print(perms)
+# print(len(perms))
 
-print(factorial(9))
+# print(factorial(9))
+
+print(how_many_permutations('zuzzurrellone'))
+
+
+
+
---
+++ README.md
@@ -0,0 +1,5 @@
+# Free Code Camp Discrete Mathematics
+
+Exercises in discrete mathematics:
+
+https://www.youtube.com/watch?v=G9JxuWk7BDA&t=1154s
